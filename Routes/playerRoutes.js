5d4d369f35ef02a9551247c1c6faf59f39598ee6
artem <artem.ip32@gmail.com>
2017-07-01 16:26:35 -0700

    moved logic from routes to playerController.js

--- Routes/playerRoutes.js
+++ Routes/playerRoutes.js
@@ -2,12 +2,8 @@ var express = require('express');
 var Player = require('../models/playerModel');
 
 var routes = function () {
-    var hockeyRouter = express.Router();
     var playerController = require('../controllers/playerController.js')(Player);
-
-    hockeyRouter.route('/')
-        .post(playerController.post)
-        .get(playerController.get);
+    var hockeyRouter = express.Router();
 
     hockeyRouter.use('/:playerId', function (req, res, next) {
         Player.findById(req.params.playerId, function (err, player) {
@@ -23,52 +19,16 @@ var routes = function () {
         });
     })
 
+    hockeyRouter.route('/')
+        .post(playerController.post)
+        .get(playerController.get);
+
     hockeyRouter.route('/:playerId')
-        .get(function (req, res) {
-            res.json(req.player);
-        })
-        .put(function (req, res) {
-            req.player.position = req.body.position;
-            req.player.id = req.body.id;
-            req.player.weight = req.body.weight;
-            req.player.height = req.body.weight;
-            req.player.imageUrl = req.body.imageUrl;
-            req.player.birthplace = req.body.birthplace;
-            req.player.age = req.body.age;
-            req.player.name = req.body.name;
-            req.player.birthdate = req.body.birthdate;
-            req.player.number = req.body.number;
-            req.player.save(function (err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    res.json(req.player);
-                }
-            });
-        })
-        .patch(function (req, res) {
-            if (req.body._id)
-                delete req.body._id;
-            for (var i in req.body) {
-                req.player[i] = req.body[i];
-            }
-            req.player.save(function (err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    res.json(req.player);
-                }
-            });
-        })
-        .delete(function (req, res) {
-            req.player.remove(function (err) {
-                if (err)
-                    res.status(500).send(err);
-                else {
-                    res.status(204).send('Removed');
-                }
-            });
-        });
+        .get(playerController.getById)
+        .put(playerController.putById)
+        .patch(playerController.patchById)
+        .delete(playerController.deleteById);
+
     return hockeyRouter;
 };
 
--- controllers/playerController.js
+++ controllers/playerController.js
@@ -19,14 +19,67 @@ var playerController = function (Player) {
             else
                 res.json(players);
         });
+
     }
+
+    var getById = (function (req, res) {
+        res.json(req.player);
+    })
+
+    var putById = (function (req, res) {
+        req.player.position = req.body.position;
+        req.player.id = req.body.id;
+        req.player.weight = req.body.weight;
+        req.player.height = req.body.weight;
+        req.player.imageUrl = req.body.imageUrl;
+        req.player.birthplace = req.body.birthplace;
+        req.player.age = req.body.age;
+        req.player.name = req.body.name;
+        req.player.birthdate = req.body.birthdate;
+        req.player.number = req.body.number;
+        req.player.save(function (err) {
+            if (err)
+                res.status(500).send(err);
+            else {
+                res.json(req.player);
+            }
+        });
+    });
+
+    var patchById = (function (req, res) {
+        if (req.body._id)
+            delete req.body._id;
+        for (var i in req.body) {
+            req.player[i] = req.body[i];
+        }
+        req.player.save(function (err) {
+            if (err)
+                res.status(500).send(err);
+            else {
+                res.json(req.player);
+            }
+        });
+    })
+
+    var deleteById = function (req, res) {
+        req.player.remove(function (err) {
+            if (err)
+                res.status(500).send(err);
+            else {
+                res.status(204).send('Removed');
+            }
+        });
+    };
 
     return {
         post: post,
-        get: get
+        get: get,
+        getById: getById,
+        putById: putById,
+        patchById: patchById,
+        deleteById: deleteById
     }
 }
-
 
 
 module.exports = playerController;
